03f70ad8640589e3f99c1402992ad6e830f2357d
yd <pokar.yashusmile@gmail.com>
2022-11-06 10:43:17 +0530

    sieve is now better

--- sieve.cpp
+++ sieve.cpp
@@ -7,7 +7,7 @@ int sieve(int x){
     vector<bool>isPrime(x+1 , true);
     for(int i = 2; i * i <= x; i++){
         if(isPrime[i]){
-            for(int j = 2*i; j <=x; j += i){
+            for(/*int j = 2*i*/ int j = i*i; j <=x; j += i){         //we can replace 2*i with i*i
                 isPrime[j] = false;
             }
         }
