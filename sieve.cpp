8c621418a085117abb53902ecb6bb8a267bede09
yd <pokar.yashusmile@gmail.com>
2022-11-06 10:37:08 +0530

    minor changes

--- sieve.cpp
+++ sieve.cpp
@@ -2,29 +2,11 @@
 #include <vector>
 using namespace std;
 
-bool prime(int x)
-{
-    if (x == 1)
-        return false;
-    if (x == 2 || x == 3)
-        return true;
-
-    if(x % 2 !=0 && x % 3 != 0){
-        for(int i = 7; i * i <= x; i = i + 6){
-            if(x % i == 0 || x % (i+2) == 0)
-                return false;
-        }
-        return true;
-    }
-    else{
-        return false;
-    }
-}
 
 int sieve(int x){
     vector<bool>isPrime(x+1 , true);
     for(int i = 2; i * i <= x; i++){
-        if(prime(i)){
+        if(isPrime[i]){
             for(int j = 2*i; j <=x; j += i){
                 isPrime[j] = false;
             }
@@ -38,6 +20,6 @@ int sieve(int x){
 }
 
 int main(){
-    sieve(10);
+    sieve(20);
     return 0;
 }
